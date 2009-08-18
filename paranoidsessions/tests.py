650f68b5b9396860d836c76e71a97de238904fad
Ryan Kelly <ryan@rfk.id.au>
2009-08-18 10:28:53 +1000

    audit and document testcases

--- paranoidsessions/tests.py
+++ paranoidsessions/tests.py
@@ -9,19 +9,27 @@ from django.conf import settings
 import paranoidsessions
 
 def test_view(request):
+    """Simple little view for testing purposes.
+
+    This adds a unique timestamp into the session if it doesn't already
+    have one; used to make sure no info leaks between sessions.
+    """
     if "timestamp" not in request.session:
         request.session["timestamp"] = time.time()
     return HttpResponse("OK")
 
-urlpatterns = patterns('',
-  (r'^$', test_view),
-  (r'^safeview$', test_view),
-)
 
 def request_filter(request):
+    """Test filtering of requests; anything with "safe" in it is filtered."""
     if "safe" in request.path:
         return False
     return True
+
+
+urlpatterns = patterns('',
+  (r'^$', test_view), 
+  (r'^safeview$', test_view),
+)
 
 
 def with_settings(**new_settings):
@@ -55,7 +63,7 @@ class TestParanoidSessions(TestCase):
 
     @with_settings(PSESSION_SECURE_COOKIE_NAME="secureid")
     def test_secure_key_handling(self):
-        #  No secure key given as it's not a secure request
+        #  No secure key gets sent as it's not a secure request
         r = self.client.get("/")
         session1 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
         self.assertFalse("secureid" in self.client.cookies)
@@ -71,6 +79,7 @@ class TestParanoidSessions(TestCase):
         self.assertEquals(session1,session3)
         self.assertFalse("secureid" in r.cookies)
         #  Insecure requests are accepted with an invalid secure key
+        #  (Django test client dosen't respect 'secure' cookie setting)
         self.client.cookies["secureid"] = "invalid"
         r = self.client.get("/")
         session4 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
@@ -89,7 +98,7 @@ class TestParanoidSessions(TestCase):
         key2 = self.client.cookies["secureid"].value
         self.assertNotEquals(session1,session5)
         self.assertNotEquals(key1,key2)
-        #  Rejected session still works OK
+        #  Rejected session is new, and works OK
         r = self.client.get("/",**{"wsgi.url_scheme":"https"})
         session6 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
         self.assertEquals(session5,session6)
@@ -98,7 +107,7 @@ class TestParanoidSessions(TestCase):
         r = self.client.get("/",**{"wsgi.url_scheme":"https"})
         session7 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
         self.assertNotEquals(session5,session7)
-        #  Check that initiating a session over https works as expected
+        #  It's possible to directly start a session over secure connection
         del self.client.cookies[settings.SESSION_COOKIE_NAME]
         r = self.client.get("/",**{"wsgi.url_scheme":"https"})
         session8 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
@@ -106,13 +115,14 @@ class TestParanoidSessions(TestCase):
         r = self.client.get("/",**{"wsgi.url_scheme":"https"})
         session9 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
         self.assertEquals(session8,session9)
-        
 
     @with_settings(PSESSION_NONCE_TIMEOUT=0)
     def test_nonce_generation(self):
+        #  We are initially assigned a nonce
         r = self.client.get("/")
         session1 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
         nonce1 = self.client.cookies[settings.PSESSION_COOKIE_NAME].value
+        #  And the next request gets a new one
         r = self.client.get("/")
         session2 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
         nonce2 = self.client.cookies[settings.PSESSION_COOKIE_NAME].value
@@ -121,11 +131,13 @@ class TestParanoidSessions(TestCase):
 
     @with_settings(PSESSION_NONCE_TIMEOUT=0.2)
     def test_nonce_timeout(self):
+        #  We keep the same nonce for a few requests
         r = self.client.get("/")
         nonce1 = self.client.cookies[settings.PSESSION_COOKIE_NAME].value
         r = self.client.get("/")
         nonce2 = self.client.cookies[settings.PSESSION_COOKIE_NAME].value
         self.assertEqual(nonce1,nonce2)
+        #  But get a new one after the timeout
         time.sleep(0.2)
         r = self.client.get("/")
         nonce2 = self.client.cookies[settings.PSESSION_COOKIE_NAME].value
@@ -133,6 +145,7 @@ class TestParanoidSessions(TestCase):
 
     @with_settings(PSESSION_NONCE_TIMEOUT=0)
     def test_invalid_nonce(self):
+        #  Proving proper nonces gives access to our session data
         r = self.client.get("/")
         session1 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
         timestamp1 = self.client.session["timestamp"]
@@ -141,6 +154,7 @@ class TestParanoidSessions(TestCase):
         timestamp2 = self.client.session["timestamp"]
         self.assertEqual(session1,session2)
         self.assertEqual(timestamp1,timestamp2)
+        #  But an invalid nonce gets us booted into a fresh session
         self.client.cookies[settings.PSESSION_COOKIE_NAME].value = "invalid"
         r = self.client.get("/")
         session3 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
@@ -150,6 +164,7 @@ class TestParanoidSessions(TestCase):
 
     @with_settings(PSESSION_NONCE_WINDOW=2,PSESSION_NONCE_TIMEOUT=0)
     def test_nonce_window(self):
+        #  Generate two nonces, we'll use the old one for requests
         r = self.client.get("/")
         nonce1 = self.client.cookies[settings.PSESSION_COOKIE_NAME].value
         session1 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
@@ -175,6 +190,7 @@ class TestParanoidSessions(TestCase):
 
     @with_settings(PSESSION_NONCE_WINDOW=3,PSESSION_NONCE_TIMEOUT=0,PSESSION_NONCE_WINDOW_TIMEOUT=0.2)
     def test_nonce_window_timeout(self):
+        #  Generate two nonces, we'll use the old one for requests
         r = self.client.get("/")
         nonce1 = self.client.cookies[settings.PSESSION_COOKIE_NAME].value
         session1 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
@@ -187,7 +203,7 @@ class TestParanoidSessions(TestCase):
         r = self.client.get("/")
         session3 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
         self.assertEqual(session1,session3)
-        #  Wait for timeout, then it should fail
+        #  After the window timeout has elapsed, we get our session booted.
         time.sleep(0.2)
         self.client.cookies[settings.PSESSION_COOKIE_NAME].value = nonce1
         r = self.client.get("/")
@@ -196,14 +212,17 @@ class TestParanoidSessions(TestCase):
 
     @with_settings(PSESSION_KEY_TIMEOUT=0.2)
     def test_key_timeout(self):
+        # No nonces here, just check the session key.
         r = self.client.get("/")
         session1 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
         timestamp1 = self.client.session["timestamp"]
+        # Session key stays the same for a while
         r = self.client.get("/")
         session2 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
         timestamp2 = self.client.session["timestamp"]
         self.assertEqual(session1,session2)
         self.assertEqual(timestamp1,timestamp2)
+        # But is cycled after the timeout has elapsed
         time.sleep(0.2)
         r = self.client.get("/")
         session3 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
@@ -252,11 +271,13 @@ class TestParanoidSessions(TestCase):
         r = self.client.get("/")
         session1 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
         nonce1 = self.client.cookies[settings.PSESSION_COOKIE_NAME].value
+        #  Requests to the safe view aren't validated
         r = self.client.get("/safeview",HTTP_USER_AGENT="attacker")
         session2 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
         nonce2 = self.client.cookies[settings.PSESSION_COOKIE_NAME].value
         self.assertEqual(session1,session2)
         self.assertEqual(nonce1,nonce2)
+        #  But requests to the other view get booted
         r = self.client.get("/",HTTP_USER_AGENT="attacker")
         session2 = self.client.cookies[settings.SESSION_COOKIE_NAME].value
         nonce2 = self.client.cookies[settings.PSESSION_COOKIE_NAME].value
